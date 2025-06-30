71fb8df9bb07df0b21d075ccefc74e65a111b3af
Geovanny Silva Ribeiro <geovanny.ribeiro@gmail.com>
2025-06-30 08:55:39 -0300

    fix: adjusting to run Plop CLI when invoke package

--- plopfile.js
+++ plopfile.js
@@ -1,45 +1,43 @@
 #!/usr/bin/env node
 const { pascalCase, camelCase } = require('change-case');
-const nodePlop = require('plop');
+
+const plop = require('plop');
 const path = require('path');
+const plopfile = plop(path.resolve(__dirname, 'plopfile.js'));
 
-module.exports = function (plop) {
-  plop.setGenerator('repository', {
-    description: 'Gera módulo, service e repositório com BaseRepository e GenericRepository',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Nome da entidade (ex: user):',
-      },
-    ],
-    actions: function (data) {
-      return [
-        {
-          type: 'add',
-          path: 'src/{{lowerCase name}}/{{lowerCase name}}.entity.ts',
-          templateFile: 'plop-templates/entity.hbs',
-        },
-        {
-          type: 'add',
-          path: 'src/{{lowerCase name}}/{{lowerCase name}}.repository.ts',
-          templateFile: 'plop-templates/repository.hbs',
-        },
-        {
-          type: 'add',
-          path: 'src/{{lowerCase name}}/{{lowerCase name}}.service.ts',
-          templateFile: 'plop-templates/service.hbs',
-        },
-        {
-          type: 'add',
-          path: 'src/{{lowerCase name}}/{{lowerCase name}}.module.ts',
-          templateFile: 'plop-templates/module.hbs',
-        },
-      ];
+plopfile.setGenerator('repository', {
+  description: 'Gera módulo, service e repositório com BaseRepository e GenericRepository',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Nome da entidade (ex: user):',
     },
-  });
-};
+  ],
+  actions: function (data) {
+    return [
+      {
+        type: 'add',
+        path: 'src/{{lowerCase name}}/{{lowerCase name}}.entity.ts',
+        templateFile: 'plop-templates/entity.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/{{lowerCase name}}/{{lowerCase name}}.repository.ts',
+        templateFile: 'plop-templates/repository.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/{{lowerCase name}}/{{lowerCase name}}.service.ts',
+        templateFile: 'plop-templates/service.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/{{lowerCase name}}/{{lowerCase name}}.module.ts',
+        templateFile: 'plop-templates/module.hbs',
+      },
+    ];
+  },
+});
 
-if (require.main === module) {
-  require('plop/lib/cli')();
-}
+require('plop/lib/cli')();
