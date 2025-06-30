fd39fb4b29dad8baa131ca04c0e773a530f9c5c0
Geovanny Silva Ribeiro <geovanny.ribeiro@gmail.com>
2025-06-30 09:07:48 -0300

    fix: adjusting to run Plop CLI when invoke package

--- plopfile.js
+++ plopfile.js
@@ -2,10 +2,17 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
+const userCwd = process.cwd();
+
+const plopfile = path.join(__dirname, 'plopfile-base.js');
+
 const plopProcess = spawn(
   process.platform === 'win32' ? 'npx.cmd' : 'npx',
-  ['plop', '--plopfile', path.join(__dirname, 'plopfile-base.js')],
-  { stdio: 'inherit' }
+  ['plop', '--plopfile', plopfile],
+  {
+    cwd: userCwd,     
+    stdio: 'inherit', 
+  }
 );
 
 plopProcess.on('exit', (code) => {
